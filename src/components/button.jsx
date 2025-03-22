1a9b90542b96e8ee415fa605280b1066f88fd7fe
Harshith Renikindi <harshith.renikidi@kalvium.community>
2025-03-22 15:18:04 +0530

    Done

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,17 @@
-// write button card here
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css"; // Import external CSS file for button styling
+
+const ViewProductButton = ({ onClick }) => {
+  return (
+    <button className="view-product-btn" onClick={onClick}>
+      View Product
+    </button>
+  );
+};
+
+ViewProductButton.propTypes = {
+  onClick: PropTypes.func,
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+import React from "react";
+import ViewProductButton from "../components/button"; 
+import "./Productcard.css"; 
+
+const Productcard = () => {
+  
+  const productImage = "https://tmart-8.myshopify.com/cdn/shop/products/04_cd10a204-6265-416d-a026-5f9ef75f611d_grande.jpg?v=1517554070";
+  const productName = "Sample Product";
+  const productPrice = "$50.00";
+
+  return (
+    <div className="product-card">
+      <img src={productImage} alt="Product" className="product-image" />
+      <h2 className="product-name">{productName}</h2>
+      <p className="product-price">{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default Productcard;
